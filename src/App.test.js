a42f6270c41d25c2ec487d7483288f23d043fb9d
Wesley Ferreira <wsf.ley@gmail.com>
2020-03-23 18:20:54 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement ).toBeInTheDocument();
+  expect("valor alterado").toBeInTheDocument();
 });
 
 
